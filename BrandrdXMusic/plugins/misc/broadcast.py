0035706d569b4b4730573803d9e13ce8e3e26d38
Mohammed Rafiq H <120514001+Padget123@users.noreply.github.com>
2024-01-24 08:00:40 +0530

    Update broadcast.py

--- BrandrdXMusic/plugins/misc/broadcast.py
+++ BrandrdXMusic/plugins/misc/broadcast.py
@@ -25,7 +25,7 @@ IS_BROADCASTING = False
 async def braodcast_message(client, message, _):
     if message.from_user.id != OWNER_ID:
         return await message.reply_text(
-            "» **sɪʀғ ʏᴇʜ @BRANDRD_BOT ʙʀᴏᴀᴅᴄᴀsᴛ ᴋᴀʀ sᴀᴋᴛᴀ ʜᴀɪ 😏**\n» ᴊᴏɪɴ @BRANDED_WORLD ғᴏʀ ᴘʀᴏᴍᴏ"
+            "» **ᴏɴʟʏ ᴀᴅᴍɪɴs ᴄᴀɴ ʙʀᴏᴀᴅᴄᴀsᴛ ᴊᴏɪɴ @friendhshipworldgroup ғᴏʀ ᴍᴏʀᴇ"
         )
     global IS_BROADCASTING
     if message.reply_to_message:
